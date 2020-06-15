6fd338123b353576a5f6ea61bc10f146040f67ec
Andrea Machado <andreamgaropaba@gmail.com>
2020-06-15 15:31:18 -0300

    Ajustando o APP.

--- src/main/java/br/com/codenation/senha/service/SenhaServiceImpl.java
+++ src/main/java/br/com/codenation/senha/service/SenhaServiceImpl.java
@@ -1,7 +1,9 @@
 package br.com.codenation.senha.service;
 
 import br.com.codenation.senha.model.Senha;
+import br.com.codenation.senha.model.SenhaHistorico;
 import br.com.codenation.senha.model.TipoSenha;
+import br.com.codenation.senha.repository.SenhaHistoricoRepository;
 import br.com.codenation.senha.repository.SenhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -10,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,6 +21,8 @@ public class SenhaServiceImpl implements SenhaService {
 
     @Autowired
     private SenhaRepository senhaRepository;
+
+    private SenhaHistoricoRepository senhaHistoricoRepository;
 
     @Override
     public List<Senha> findAll(Pageable pageable) {
@@ -45,8 +50,38 @@ public class SenhaServiceImpl implements SenhaService {
     }
 
     @Override
-    public List<Senha> findByTipoSenha(TipoSenha tipoSenha, Pageable pageable) {
-        return this.senhaRepository.findByTipoSenha(tipoSenha,pageable);
+    public Senha findByTipoSenha(TipoSenha tipoSenha) {
+
+        Long proximaSenha = senhaRepository.findByTipoSenha(tipoSenha).getProximoNumero();
+
+        //Pega a próxima senha, atualiza a tabela de senha e gera um registro
+        //na tabela de historico de senha.
+        Senha senha = new Senha();
+        senha.setTipoSenha(tipoSenha);
+        senha.setProximoNumero(proximaSenha+1);
+        senhaRepository.save(senha);
+
+        SenhaHistorico senhaHistorico = new SenhaHistorico();
+        senhaHistorico.setDataGeracao(LocalDateTime.now());
+        senhaHistorico.setNumero(proximaSenha+1);
+        senhaHistorico.setTipoSenha(tipoSenha);
+        senhaHistoricoRepository.save(senhaHistorico);
+
+        return senha;
     }
+
+    @Override
+    public Senha findProximaSenha(TipoSenha tipoSenha) {
+        Long proximaSenha = senhaRepository.findProximaSenha(tipoSenha).getProximoNumero();
+
+        Senha senha = new Senha();
+        senha.setTipoSenha(tipoSenha);
+        senha.setProximoNumero(proximaSenha+1);
+        //senhaRepository.updateProximaSenha(senha);
+
+        return senhaRepository.save(senha);
+
+    }
+
 
 }
